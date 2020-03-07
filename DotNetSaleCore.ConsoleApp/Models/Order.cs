aa9267659b029c516e5e28370650df4ce096979e
RedPlus <redplus@redplus.net>
2020-03-08 08:10:36 +0900

    초기 커밋.

---
+++ DotNetSaleCore.ConsoleApp/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNetSaleCore.ConsoleApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime? OrderFulfilled { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
---
+++ DotNetSaleCore.ConsoleApp/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNetSaleCore.ConsoleApp.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        public ICollection<DotNetSaleCore.ConsoleApp.Models.OrderDetails> OrderDetails { get; set; }
+    }
+}
